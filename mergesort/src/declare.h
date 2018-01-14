e85698d3c740f14992bec254f4bde5ffbf6645f6
Luis E. Cerna <cernalue@gmail.com>
2018-01-14 10:27:01 -0600

    Wrote functions: 'allocation', 'File_manage', 'merge_sort' and 'copy_arr'. Haven tested if they work, but code compiles.

--- mergesort/src/declare.h
+++ mergesort/src/declare.h
@@ -2,4 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-
+void allocation(double **array, unsigned int N_ofelements);
+void File_manage(char **src, double *dst, unsigned int N_ofelements);
+unsigned int merge_sort(double *arr, unsigned int N_ofelements);
+void copy_arr(double *src, double *dst, unsigned int N_ofelements);
--- mergesort/src/mergesort.c
+++ mergesort/src/mergesort.c
@@ -1,28 +1,64 @@
 #include "declare.h"
 
 int main(int argc, char** argv)
-{ 
-int N_ofelements;
+{
+unsigned int N_ofelements;
+double *arr;
+
 N_ofelements = atoi( argv[2] );
-double *arr = malloc( N_ofelements * sizeof(double) );
-FILE *fp;
-fp = fopen(argv[1], "r");
+//allocation(double **array, unsigned int N_ofelements);
+allocation(&arr, N_ofelements);
+File_manage(argv, arr, N_ofelements);
 
-if(*arr == NULL)
-  {
-  printf("\n\nError during memory allocation.\n\n");
-  }
-if(fp == NULL)
-  {  
-  printf("Error opening sorting file");
-  }
-
-for(int i = 0; i < N_ofelements; i++)
-  {
-  fscanf(fp, "%lf\n", &arr[i] );
-  }
-
-fclose(fp);
 free(arr);
 return(0);
 }
+
+void allocation(double **array, unsigned int N_ofelements)
+{
+*array = malloc( N_ofelements * sizeof(double) );
+if(*array == NULL)
+  {
+  printf("\n\nError during memory allocation\n\n"\
+  "in 'allocation()' function.\n\n");
+  }
+}
+
+void File_manage(char **src, double *dst, unsigned int N_ofelements)
+{
+unsigned int i;
+FILE *fp;
+fp = fopen(src[1], "r");
+if(fp == NULL)
+  {  
+  printf("\n\nError opening sorting file\n"\
+  "in 'File_manage()'.\n\n");
+  exit(0);
+  }
+for(i = 0; i < N_ofelements; i++)
+  {
+  fscanf(fp, "%lf\n", &dst[i] );
+  }
+fclose(fp);
+}
+
+unsigned int merge_sort(double *arr, unsigned int N_ofelements)
+{
+double *buffer;
+unsigned int N_ofoperations = 0;
+copy_arr(arr, buffer, N_ofelements);
+//operations = N + split_merge_sorted();
+free(buffer);
+return(N_ofoperations);
+}
+
+void copy_arr(double *src, double *dst, unsigned int N_ofelements)
+{
+unsigned int i;
+for(i = 0; i < N_ofelements; i++)
+  {
+  dst[i] = src[i];
+  }
+}
+
+
