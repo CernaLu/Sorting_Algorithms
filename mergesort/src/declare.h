7f60d4eea310738ebe8ee0e8d61c240ee261a851
Luis E. Cerna <cernalue@gmail.com>
2018-01-17 18:58:42 -0600

    Fixed all 'Segmentation fault: 11' errors.

--- mergesort/src/declare.h
+++ mergesort/src/declare.h
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-unsigned int allocation(double **array, unsigned int N_ofelements);
+unsigned int allocate(double **array, unsigned int N_ofelements);
 unsigned int File_manage(char **src, double *dst, unsigned int N_ofelements);
 unsigned int copy_arr(double *src, double *dst, unsigned int N_ofelements);
 unsigned int merge_sort(double *arr, unsigned int N_ofelements);
--- mergesort/src/mergesort.c
+++ mergesort/src/mergesort.c
@@ -12,7 +12,7 @@ double *arr;
 N_ofelements = atoi( argv[2] );
 printf("\n\nExit code success only means that a function was exit successfully,\n"\
        "but doesn't mean that the function did its work properly");
-ID0 = allocation(&arr, N_ofelements);
+ID0 = allocate(&arr, N_ofelements);
 printf("\n\nAllocation succesfull. Exit code %d.", ID0);
 ID1 = File_manage(argv, arr, N_ofelements);
 printf("\n\nFILE manage succesfull. Exit code %d.", ID1);
@@ -20,10 +20,10 @@ IDf = merge_sort(arr, N_ofelements);
 printf("\nMerge sort succesfull. Exit code %d.", IDf);
 
 free(arr);
-return(0);
+return(6);
 }
 
-unsigned int allocation(double **array, unsigned int N_ofelements)
+unsigned int allocate(double **array, unsigned int N_ofelements)
 {
 *array = malloc( N_ofelements * sizeof(double) );
 if(*array == NULL)
@@ -46,22 +46,16 @@ if(fp == NULL)
   "in 'File_manage()'.\n\n");
   exit(0);
   }
-
-printf("%d", N_ofelements);
 for(i = 0; i < N_ofelements; i++)
   {
-  printf("\n\n%d\n\n", i);
   fscanf(fp, "%lf\n", &dst[i] );
-  printf("\n\n%lf\n\n", dst[i]);
   }
 
-printf("TEST1");
 fclose(fp);
-printf("TEST2");
-return(0);
+return(1);
 }
 
-unsigned int copy_arr(double *src, double **dst, unsigned int N_ofelements)
+unsigned int copy_arr(double *src, double *dst, unsigned int N_ofelements)
 {
 unsigned int i;
 for(i = 0; i < N_ofelements; i++)
@@ -74,13 +68,16 @@ return(2);
 unsigned int merge_sort(double *arr, unsigned int N_ofelements)
 {
 unsigned int ID2 = 0;
-unsigned int ID4 = 0;
+unsigned int ID3 = 0;
+unsigned int ID5 = 0;
 double *buffer;
 
-ID2 = copy_arr(arr, &buffer, N_ofelements);
-printf("\n\nCopy of arr succesfull. Exit code %d.", ID2);
-ID4 = split_merge_sorted(buffer, arr, 0, N_ofelements-1);
-printf("\n\nSplit merge sorted success. Exit code %d.", ID4);
+ID2 = allocate(&buffer, N_ofelements);
+printf("\n\nAllocation succesfull. Exit code %d.", ID2);
+ID3 = copy_arr(arr, buffer, N_ofelements);
+printf("\n\nCopy of arr succesfull. Exit code %d.", ID3);
+ID5 = split_merge_sorted(buffer, arr, 0, N_ofelements-1);
+printf("\n\nSplit merge sorted success. Exit code %d.", ID5);
 
 free(buffer);
 return(5);
@@ -89,20 +86,27 @@ return(5);
 unsigned int split_merge_sorted(double *src, double *dst,\
                                 unsigned int firstindex, unsigned int lastindex)
 {
-unsigned int ID3 = 0;
+unsigned int op = 0;
+unsigned int ID4 = 0;
 unsigned int middleindex;
+
+if(lastindex - firstindex < 1)
+{
+return(0);
+}
 
 middleindex = (firstindex + lastindex) / 2;
 
-split_merge_sorted(dst, src, firstindex, middleindex);
-split_merge_sorted(dst, src, middleindex+1, lastindex);
-ID3 = merge_sorted(src, dst, firstindex, middleindex, lastindex);
-printf("\n\nMerge sorted success. Exit code %d.", ID3);
+op += split_merge_sorted(dst, src, firstindex, middleindex);
+op += split_merge_sorted(dst, src, middleindex+1, lastindex);
+op += merge_sorted(src, dst, firstindex, middleindex, lastindex);
+printf("\nNumber of operations = %d\n", op);
+printf("\n\nMerge sorted success. Exit code %d.", ID4);
 
-return(4);
+return(op);
 }
 
-unsigned int merge_sorted(double *src,double *dst,\
+unsigned int merge_sorted(double *src, double *dst,\
                           unsigned int firstindex, unsigned int middleindex,\
                           unsigned int lastindex)
 {
@@ -142,5 +146,5 @@ for(dstindex = firstindex; dstindex <= lastindex; dstindex++)
     }
   sorted_numbers++;
   }
-return(3);
+return(sorted_numbers++);
 }
